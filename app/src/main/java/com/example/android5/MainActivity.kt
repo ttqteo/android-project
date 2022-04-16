b95f75505c5835b0153f9a64087a1011e8b85e71
nglequy <nglequy@email.com>
2022-04-16 15:42:36 +0700

    fix bottom nav

--- app/src/main/java/com/example/android5/MainActivity.kt
+++ app/src/main/java/com/example/android5/MainActivity.kt
@@ -3,19 +3,39 @@ package com.example.android5
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.android5.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val scheduleFragment = ScheduleFragment()
-    private val newsfeedFragment = NewFragment()
+    private val newFragment = NewFragment()
     private val notificationFragment = NotificationFragment()
     private val profileFragment = ProfileFragment()
 
+    private lateinit var bottomNavView: BottomNavigationView
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        bottomNavView = binding.botNav
+
         replaceFragment(homeFragment)
+        bottomNavView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home_bot -> replaceFragment(homeFragment)
+                R.id.schedule_bot -> replaceFragment(scheduleFragment)
+                R.id.new_bot -> replaceFragment(newFragment)
+                R.id.notification_bot -> replaceFragment(notificationFragment)
+                R.id.profile_bot -> replaceFragment(profileFragment)
+            }
+            true
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/com/example/android5/NewFragment.kt
+++ app/src/main/java/com/example/android5/NewFragment.kt
@@ -18,7 +18,7 @@ class NewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_newfeed, container, false)
+        return inflater.inflate(R.layout.fragment_new, container, false)
     }
 }
 
